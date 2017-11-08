84b4c83c56975c1cb63ad07a549e6ea62d64c0b0
varunhawk19 <varunhawk19@gmail.com>
2017-11-08 21:30:32 +0530

    new update

--- main_gentic_algo_hash_creator.cpp
+++ main_gentic_algo_hash_creator.cpp
@@ -8,11 +8,108 @@ public:
 };
 
 struct Population {
-	vector<Chromosome> Chromosomes = vector<Chromosome>(500);
+	vector<Chromosome> Chromosomes = vector<Chromosome>(5);
 }Pop;
 
 int prime[25]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
 
+unsigned int RSHash(const char* str, unsigned int length)
+{
+   unsigned int b    = 378551;
+   unsigned int a    = 63689;
+   unsigned int hash = 0;
+   unsigned int i    = 0;
+
+   for (i = 0; i < length; ++str, ++i)
+   {
+      hash = hash * a + (*str);
+      a    = a * b;
+   }
+
+   return hash;
+}
+
+unsigned int JSHash(const char* str, unsigned int length)
+{
+   unsigned int hash = 1315423911;
+   unsigned int i    = 0;
+
+   for (i = 0; i < length; ++str, ++i)
+   {
+      hash ^= ((hash << 5) + (*str) + (hash >> 2));
+   }
+
+   return hash;
+}
+unsigned int PJWHash(const char* str, unsigned int length)
+{
+   const unsigned int BitsInUnsignedInt = (unsigned int)(sizeof(unsigned int) * 8);
+   const unsigned int ThreeQuarters     = (unsigned int)((BitsInUnsignedInt  * 3) / 4);
+   const unsigned int OneEighth         = (unsigned int)(BitsInUnsignedInt / 8);
+   const unsigned int HighBits          =
+                      (unsigned int)(0xFFFFFFFF) << (BitsInUnsignedInt - OneEighth);
+   unsigned int hash = 0;
+   unsigned int test = 0;
+   unsigned int i    = 0;
+
+   for (i = 0; i < length; ++str, ++i)
+   {
+      hash = (hash << OneEighth) + (*str);
+
+      if ((test = hash & HighBits) != 0)
+      {
+         hash = (( hash ^ (test >> ThreeQuarters)) & (~HighBits));
+      }
+   }
+
+   return hash;
+}
+
+unsigned int ELFHash(const char* str, unsigned int length)
+{
+   unsigned int hash = 0;
+   unsigned int x    = 0;
+   unsigned int i    = 0;
+
+   for (i = 0; i < length; ++str, ++i)
+   {
+      hash = (hash << 4) + (*str);
+
+      if ((x = hash & 0xF0000000L) != 0)
+      {
+         hash ^= (x >> 24);
+      }
+
+      hash &= ~x;
+   }
+
+   return hash;
+}
+unsigned int BKDRHash(const char* str, unsigned int length)
+{
+   unsigned int seed = 131; /* 31 131 1313 13131 131313 etc.. */
+   unsigned int hash = 0;
+   unsigned int i    = 0;
+
+   for (i = 0; i < length; ++str, ++i)
+   {
+      hash = (hash * seed) + (*str);
+   }
+
+   return hash;
+}
+unsigned int SDBMHash(const char* str, unsigned int length)
+{
+   unsigned int hash = 0;
+   unsigned int i    = 0;
+
+   for (i = 0; i < length; ++str, ++i)
+   {
+      hash = (*str) + (hash << 6) + (hash << 16) - hash;
+   }
+
+   return hash;
+}
 unsigned int DEKHash(const char* str, unsigned int length)
 {
    unsigned int hash_ = length;
@@ -69,7 +166,7 @@ string get_seed()
 
 int main() {
      ofstream f("random.txt",ios_base::app);
-    unsigned int hashed_seed_1,hashed_seed_2,hashed_seed_3,hashed_seed_4;
+    vector <unsigned int> hashed_seed;
     hash <string> hash_fn; //only required if secondhashed
     int total_number;
     cout<<"enter the number random numbers that you require";
@@ -108,13 +205,17 @@ int main() {
 			}
 		}
     if(total_number>0)
-    {hashed_seed_1=(APHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()));
-    hashed_seed_2=(hash_fn(Parents.at(0).gene));
-    hashed_seed_3=(DEKHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()));
-    hashed_seed_4=(DJBHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()));
-    f<<hashed_seed_1+hashed_seed_2+hashed_seed_3+hashed_seed_4<<'\n';
-    cout<<total_number<<"\t:"<<hashed_seed_1+hashed_seed_2+hashed_seed_3+hashed_seed_4<<'\n';
-    --total_number;
+    {f<<(APHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()))<<"\n";
+    f<<(hash_fn(Parents.at(0).gene))<<"\n";
+    f<<(DEKHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()))<<"\n";
+    f<<(DJBHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()))<<"\n";
+    f<<(RSHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()))<<"\n";
+    f<<(JSHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()))<<"\n";
+    f<<(PJWHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()))<<"\n";
+    f<<(ELFHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()))<<"\n";
+    f<<(SDBMHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()))<<"\n";
+    f<<(BKDRHash((Parents.at(0).gene).c_str(),Parents.at(0).gene.length()))<<"\n";
+    total_number-=10;
     }
 	}
     }
